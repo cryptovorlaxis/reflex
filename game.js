1841e908a763f459729dd620c4b239567db617b3
Cryptovorlaxis <207086501+cryptovorlaxis@users.noreply.github.com>
2025-12-07 22:21:57 +0300

    Update game.js

--- game.js
+++ game.js
@@ -32,7 +32,7 @@ let isSharing = false;
 
 // ---- Payment yapılandırması ----
 const PAYMENT_RECEIVER = "0xe1E6565AD8d30A5Cd9D93cCe79e160d014F762A5";
-const PAYMENT_AMOUNT_WEI_HEX = "0x5af3107a4000"; // 0.0001 ETH (~0.30 USD)
+const PAYMENT_AMOUNT_WEI_HEX = "0x46186ac000"; // 0.0001 ETH (~0.30 USD)
 const BASE_CHAIN_ID_HEX = "0x2105"; // 8453 Base Mainnet
 
 // ================================================================
